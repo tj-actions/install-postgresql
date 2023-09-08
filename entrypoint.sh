3baac232abc87b37dc2b91aa63b54ebd77e14a67
Tonye Jack <jtonye@ymail.com>
2023-09-08 04:23:04 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,7 +37,7 @@ if [[ "$(uname -s)" == "Linux" ]]; then
     # Install PostgreSQL
     sudo apt-get install -y "postgresql-$INPUT_POSTGRESQL_VERSION"
 elif [[ "$(uname -s)" == "NT"* ]] || [[ "$(uname -s)" == "MINGW"* ]] || [[ "$(uname -s)" == *"MSYS"* ]]; then
-    choco install postgresql --version="$INPUT_POSTGRESQL_VERSION" -y --source chocolatey
+    choco install "postgresql$INPUT_POSTGRESQL_VERSION" -y
 elif [[ "$(uname -s)" == "Darwin" ]]; then
     brew update
     brew install "postgresql@$INPUT_POSTGRESQL_VERSION"
