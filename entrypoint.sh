c2c46a23cd560b986312e888fd3298e8ed28db03
Tonye Jack <jtonye@ymail.com>
2023-09-11 20:59:46 -0600

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,11 +52,11 @@ echo "Verifying installation..."
 
 # Verify installation by running pg_dump directly
 if [[ "$(uname -s)" == "NT"* ]] || [[ "$(uname -s)" == "MINGW"* ]] || [[ "$(uname -s)" == *"MSYS"* ]]; then
-    echo "C:\\Program Files\\PostgreSQL\\$INPUT_POSTGRESQL_VERSION\\bin" >> $GITHUB_PATH
+    echo "C:\\Program Files\\PostgreSQL\\$INPUT_POSTGRESQL_VERSION\\bin" >> "$GITHUB_PATH"
 elif [[ "$(uname -s)" == "Darwin" ]]; then
-    echo "/usr/local/opt/postgresql@${INPUT_POSTGRESQL_VERSION}/bin" >> $GITHUB_PATH
+    echo "/usr/local/opt/postgresql@${INPUT_POSTGRESQL_VERSION}/bin" >> "$GITHUB_PATH"
 else
-    echo "/usr/lib/postgresql/$INPUT_POSTGRESQL_VERSION/bin" >> $GITHUB_PATH
+    echo "/usr/lib/postgresql/$INPUT_POSTGRESQL_VERSION/bin" >> "$GITHUB_PATH"
 fi
 
 echo "Complete"
