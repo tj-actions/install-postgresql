72dd9bcd9541c3e5c70bc5f88c3f1b3d257590d8
Tonye Jack <jtonye@ymail.com>
2023-09-12 10:10:25 -0600

    Update the cache-location

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,7 +37,7 @@ if [[ "$(uname -s)" == "Linux" ]]; then
     # Install PostgreSQL
     sudo apt-get apt-get -o Dir::Cache::Archives=/tmp install -y "postgresql-$INPUT_POSTGRESQL_VERSION"
 elif [[ "$(uname -s)" == "NT"* ]] || [[ "$(uname -s)" == "MINGW"* ]] || [[ "$(uname -s)" == *"MSYS"* ]]; then
-    choco install "postgresql$INPUT_POSTGRESQL_VERSION" -y --no-progress
+    choco install "postgresql$INPUT_POSTGRESQL_VERSION" -y --no-progress --cache-location="C:\\ProgramData\\chocolatey\\cache"
 elif [[ "$(uname -s)" == "Darwin" ]]; then
     brew update
     brew install "postgresql@$INPUT_POSTGRESQL_VERSION"
