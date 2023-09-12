8e47058fa9ed23724032d8ebd6d5038b0354d13f
Tonye Jack <jtonye@ymail.com>
2023-09-12 11:11:05 -0600

    Updated the PATH regardless of cache hit outcome

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,18 +48,4 @@ fi
 
 echo "Installed postgresql"
 
-echo "Updating PATH..."
-
-# Verify installation by running pg_dump directly
-if [[ "$(uname -s)" == "NT"* ]] || [[ "$(uname -s)" == "MINGW"* ]] || [[ "$(uname -s)" == *"MSYS"* ]]; then
-    # shellcheck disable=SC2028
-    echo "C:\\Program Files\\PostgreSQL\\$INPUT_POSTGRESQL_VERSION\\bin" >> "$GITHUB_PATH"
-elif [[ "$(uname -s)" == "Darwin" ]]; then
-    echo "/usr/local/opt/postgresql@${INPUT_POSTGRESQL_VERSION}/bin" >> "$GITHUB_PATH"
-else
-    echo "/usr/lib/postgresql/$INPUT_POSTGRESQL_VERSION/bin" >> "$GITHUB_PATH"
-fi
-
-echo "Complete"
-
 echo "::endgroup::"
