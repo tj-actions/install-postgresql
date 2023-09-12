9dc2432a2aefc53573a15645680e0f77aeb7a28c
Tonye Jack <jtonye@ymail.com>
2023-09-12 07:19:17 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,6 +52,7 @@ echo "Verifying installation..."
 
 # Verify installation by running pg_dump directly
 if [[ "$(uname -s)" == "NT"* ]] || [[ "$(uname -s)" == "MINGW"* ]] || [[ "$(uname -s)" == *"MSYS"* ]]; then
+    # shellcheck disable=SC2028
     echo "C:\\Program Files\\PostgreSQL\\$INPUT_POSTGRESQL_VERSION\\bin" >> $GITHUB_PATH
 elif [[ "$(uname -s)" == "Darwin" ]]; then
     echo "/usr/local/opt/postgresql@${INPUT_POSTGRESQL_VERSION}/bin" >> $GITHUB_PATH
